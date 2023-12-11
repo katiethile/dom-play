3393b7adda193707a7f1ebf79710bfcd9c0ff61a
Katie Le <lethikatie@gmail.com>
2023-12-11 15:28:19 -0800

    adds function to dom-play file and background colors for hamlet and skull

---
+++ js/dom-play.js
@@ -0,0 +1,13 @@
+//alert("hello from DOM Play")
+
+//document.querySelector(".hamlet").style.backgroundColor = "blue";
+
+function highlight(el){
+
+    if(el.style.backgroundColor == 'orange'){
+        el.style.backgroundColor='white';
+    }else{
+        el.style.backgroundColor='orange';
+    
+    }    
+}
