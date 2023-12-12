08ba9b2524d7d0579367a2413aadebf2c8e241a7
Katie Le <lethikatie@gmail.com>
2023-12-12 15:26:55 -0800

    adds functions to page to highlight what actors are saying

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,6 +1,24 @@
 //alert("hello from DOM Play")
 
-//document.querySelector(".hamlet").style.backgroundColor = "blue";
+const spans = document.querySelectorAll("#play span");
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click",function(ev){
+            highlightActor(mySpan.dataset.actor);
+    });
+}
+
+function highlightActor(actor){
+   // alert("I'm " + actor);
+   for(const mySpan of spans){
+    if(actor == mySpan.dataset.actor){
+        mySpan.style.backgroundColor='orange';
+    }else{
+        mySpan.style.backgroundColor='white';
+    }    
+   }
+}
 
 function highlight(el){
 
